3168abfdc611ceb1ac9247a617b64ba2d8e009e7
Beichen <beiczhang@gmail.com>
2017-08-13 19:28:35 +0800

    this program is to calculate all the prime numbers

--- zbc.c
+++ zbc.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
+int prime(int x)
+{
+	if(x <= 1)
+		return 0;
+	int i,j = x/2;
+	for(i = 2; i<j; i++)
+	{
+		if(x%i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+
+
 int main()
 {
+	int num;
+	printf("please input the integer: \n");
+	int i;
+	
+	scanf("%d", &num);
+	for(i = 2; i<=num; i++)
+		if(prime(i))
+			printf("%d\n", i);
+
+
 	return 0;
-	}
+}
